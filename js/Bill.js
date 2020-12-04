f1465406cd92c4e4991d14135d32cb3f5724854f
JohnT1999 <52741279+JohnT1999@users.noreply.github.com>
2020-12-05 00:01:45 +0700

    Add on functionality

---
+++ js/Bill.js
@@ -0,0 +1,7 @@
+function Bill(_taxiType, _kms, _waitingTime, _cost, _total) {
+  this.taxiType = _taxiType;
+  this.kms = _kms;
+  this.cost = _cost;
+  this.waitingTime = _waitingTime;
+  this.total = _total;
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+var bills = [];
 function taxiType() {
   var type;
   var uberX = document.getElementById("uberX").checked;
   var uberSUV = document.getElementById("uberSUV").checked;
   var uberBlack = document.getElementById("uberBlack").checked;
+
   if (uberX) type = "uberX";
   else if (uberSUV) type = "uberSUV";
   else if (uberBlack) type = "uberBlack";
@@ -31,33 +33,68 @@ function pay() {
 
     var totalDisplay = document.getElementById("xuatTien");
     var totalCost = 0;
+    var cost;
+    var waitingCost;
+    var chosenTaxi = "";
+
     switch (taxi) {
       case "uberX":
-        if (km <= 1) totalCost = km * 8000 + waitingTime * 2000;
-        else if (km > 1 && km <= 20)
-          totalCost = 8000 + (km - 1) * 12000 + waitingTime * 2000;
-        else if (km > 20)
+        chosenTaxi = "uberX";
+        waitingCost = 2000;
+        if (km <= 1) {
+          totalCost = km * 8000 + waitingTime * waitingCost;
+          cost = 8000;
+        } else if (km > 1 && km <= 20) {
+          totalCost = 8000 + (km - 1) * 12000 + waitingTime * waitingCost;
+          cost = 12000;
+        } else if (km > 20) {
           totalCost =
-            8000 + 12000 * 20 + (km - 21) * 10000 + waitingTime * 2000;
+            8000 + 12000 * 20 + (km - 21) * 10000 + waitingTime * waitingCost;
+          cost = 10000;
+        }
         break;
+
       case "uberSUV":
-        if (km <= 1) totalCost = km * 9000 + waitingTime * 3000;
-        else if (km > 1 && km <= 20)
-          totalCost = 8000 + (km - 1) * 14000 + waitingTime * 3000;
-        else if (km > 20)
+        chosenTaxi = "uberSUV";
+        waitingCost = 3000;
+        if (km <= 1) {
+          totalCost = km * 9000 + waitingTime * waitingCost;
+          cost = 9000;
+        } else if (km > 1 && km <= 20) {
+          totalCost = 8000 + (km - 1) * 14000 + waitingTime * waitingCost;
+          cost = 14000;
+        } else if (km > 20) {
           totalCost =
-            8000 + 14000 * 20 + (km - 21) * 12000 + waitingTime * 3000;
+            8000 + 14000 * 20 + (km - 21) * 12000 + waitingTime * waitingCost;
+        }
         break;
+
       case "uberBlack":
-        if (km <= 1) totalCost = km * 10000 + waitingTime * 4000;
-        else if (km > 1 && km <= 20)
-          totalCost = 10000 + (km - 1) * 16000 + waitingTime * 4000;
-        else if (km > 20)
+        chosenTaxi = "uberBlack";
+        waitingCost = 4000;
+        if (km <= 1) {
+          totalCost = km * 10000 + waitingTime * waitingCost;
+          cost = 10000;
+        } else if (km > 1 && km <= 20) {
+          totalCost = 10000 + (km - 1) * 16000 + waitingTime * waitingCost;
+          cost = 16000;
+        } else if (km > 20) {
           totalCost =
-            10000 + 16000 * 20 + (km - 21) * 14000 + waitingTime * 4000;
+            10000 + 16000 * 20 + (km - 21) * 14000 + waitingTime * waitingCost;
+          cost = 14000;
+        }
         break;
     }
 
     totalDisplay.innerHTML = totalCost;
+    console.log(chosenTaxi);
+    var bill = new Bill(chosenTaxi, km, waitingTime, cost, totalCost);
+
+    bills.push(bill);
+    console.log(bills);
   }
+}
+
+function createTable() {
+  document.getElementById("billDetail").innerHTML = "";
 }
